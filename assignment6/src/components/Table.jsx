2fe4d6cc4bf23407563568ef71d00c340f25d3de
JiaminShi1226 <shijiamin0324@gmail.com>
2023-06-16 18:31:33 -0400

    fixed the clearAll button

--- assignment6/src/components/Table.jsx
+++ assignment6/src/components/Table.jsx
@@ -16,6 +16,10 @@ function Table(props) {
   };
 
   const clearAll = () => {
+    const cells = document.getElementsByClassName("cell");
+    for (let i = 0; i < cells.length; i++) {
+      cells[i].style.backgroundColor = "transparent";
+    }
     setColor("transparent");
   };
 
