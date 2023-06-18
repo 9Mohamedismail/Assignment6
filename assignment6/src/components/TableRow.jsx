0fd7593e4b64cb8a048bc769e5bfe716cf8fd6f5
Niloy <106124483+mniloy1123@users.noreply.github.com>
2023-06-17 21:43:07 -0400

    Pass the mouseDown prop to TableCell

--- assignment6/src/components/TableRow.jsx
+++ assignment6/src/components/TableRow.jsx
@@ -3,10 +3,17 @@ import TableCell from "./TableCell";
 
 function TableRow(props) {
   let tableColumns = [];
-  for (let i = -1; i < props.columns; i++) {
-    tableColumns.push(<TableCell key={i} color={props.color} />);
+  for (let i = 0; i < props.columns; i++) {
+    tableColumns.push(
+      <TableCell 
+        key={i} 
+        color={props.color} 
+        mouseDown={props.mouseDown}
+      />
+    );
   }
   return <tr>{tableColumns}</tr>;
 }
 
 export default TableRow;
+
